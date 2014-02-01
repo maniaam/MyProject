945f56a9ce166d65350d9709ab1c955fdf8c64bc
maniaam <m-almatar@neiu.edu>
2014-01-31 19:50:12 -0600

    ïnitial release

---
+++ src/myProject/YesOrNo.java
@@ -0,0 +1,23 @@
+package myProject;
+
+import javax.swing.JOptionPane;
+
+public class YesOrNo
+{
+
+	public static void main(String[] args)
+	{
+		boolean fries;
+		int  num;
+		String howMuch;
+		fries = (JOptionPane.showConfirmDialog
+				(null, "Would you like fries?","Please choose",JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION);
+		if(fries)
+		{
+			howMuch = JOptionPane.showInputDialog("How much fries would you like");
+			num = (Integer.parseInt(howMuch)) * 2;
+			JOptionPane.showMessageDialog(null, num, "The peice", JOptionPane.PLAIN_MESSAGE);
+		}
+	}
+
+}
